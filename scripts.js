a016f23768bcaf0c08f6c6c06b30c8aefd640abd
Ying-lenny <owenlenihan101@gmail.com>
2022-04-20 12:12:25 +0100

    -Added function to scripts.js to change the background colour on a mouse over
    -Added a button that doesn't do anything yet

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 const container = document.getElementById("container");
 
+let currentColor = 'black'
+
+const colors = { black: 'black' }
+
 function makeRows (rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c<(rows*cols); c++) {
         let cell = document.createElement("div");
         cell.innerText = (c+1);
-        container.appendChild(cell).className = "grid-line"
+        container.appendChild(cell).className = "grid-item"
+        
+        cell.addEventListener('mouseover',function() {
+            cell.style.backgroundColor = currentColor
+          })
     };
 };
 
