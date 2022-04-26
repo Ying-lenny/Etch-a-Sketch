9150b5d0bfbeea0ed7f1c76dc570f3908822fc1f
Ying-lenny <owenlenihan101@gmail.com>
2022-04-26 22:12:29 +0100

    Added button to change grid size Limited grid to between 1 and 100 Changed mouseover function to be a random colour

--- scripts.js
+++ scripts.js
@@ -1,21 +1,49 @@
 const container = document.getElementById("container");
+const gridButton = document.querySelector(".gridBtn");
 
-let currentColor = 'black'
+let currentColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+let grid = 16
 
 const colors = { black: 'black' }
 
-function makeRows (rows, cols) {
+function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c<(rows*cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c+1);
         container.appendChild(cell).className = "grid-item"
         
         cell.addEventListener('mouseover',function() {
             cell.style.backgroundColor = currentColor
+            refreshColour();
           })
     };
 };
 
-makeRows(16, 16);
+function refreshRows() {
+    container.replaceChildren();
+    makeRows(grid, grid);
+}
+
+function refreshColour() {
+    currentColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function createGrid() {
+    gridButton.addEventListener("click", () => {
+        grid = prompt("Please give a number between 1 and 100")
+
+        if (grid > 100) {
+            alert("Bit High init?")
+            grid = 16
+        } else if (grid <= 0) {
+            alert("Bit Low init?")
+            grid = 16
+        }
+        refreshRows()
+    })
+}
+
+makeRows(grid, grid);
+createGrid();
